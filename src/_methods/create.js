d9f465735bc087b20b37ed3d3ea56c3eaff1b4ac
Charles Matthews <charles0matthews@gmail.com>
2022-06-12 13:09:38 -0400

    update docs

--- src/_methods/create.js
+++ src/_methods/create.js
@@ -3,6 +3,15 @@
  * @param {string} type 
  * @param {object} obj 
  * @returns {object}
+ * @example
+ * let newElement = cm.create('p', {
+ *    innerHTML:'hello world',
+ *    style:{
+ *      'font-size':'16px';
+ *      }
+ *    } 
+ * );
+ * display.appendChild(newElement)
  */
 
 CM.prototype.create = function(type, obj){
