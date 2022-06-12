8be732c95f65e7fc19f9a2dbdf2dd44139a4b7c5
Charles Matthews <charles0matthews@gmail.com>
2022-06-11 20:18:06 -0400

    Update interpolate.js

--- src/methods/interpolate.js
+++ src/methods/interpolate.js
@@ -2,10 +2,10 @@
  * interpolate between two values
  * @param {number} start start point
  * @param {number} end end point
- * @param {number} n range 0-1
+ * @param {number} position range 0-1
  * @returns {number}
  */
 
-cm.prototype.interpolate = function(start, end, n) {
-    return ((1 - n) * start) + (n * end);
+cm.prototype.interpolate = function(start, end, position) {
+    return ((1 - position) * start) + (position * end);
 }
