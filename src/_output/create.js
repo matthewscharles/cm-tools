02300df38cc8ea445b554c3870d536b48b76f976
Charles Matthews <charles0matthews@gmail.com>
2022-06-14 11:32:42 -0400

    remove console.log in main file

--- src/_output/create.js
+++ src/_output/create.js
@@ -1,5 +1,5 @@
 // testing...
 let cm = new CM();
 
-console.log(cm.map(1.1, 0, 1, 0, 127, false));
-console.log(cm.interpolate(100, 200, 0.5));
+// console.log(cm.map(1.1, 0, 1, 0, 127, false));
+// console.log(cm.interpolate(100, 200, 0.5));
